168cbfd12c3aabde99756fe9bd06cc126c06462f
Raed Elsayed <Roy.Elsayed@nhsbt.nhs.uk>
2022-10-28 16:29:52 +0100

    update repo

---
+++ src/main/java/com/nhsbt/bookstore/model/Language.java
@@ -0,0 +1,16 @@
+package com.nhsbt.bookstore.model;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+
+public enum Language {
+
+    // ======================================
+    // =             Constants              =
+    // ======================================
+
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINISH, GERMAN, DUTCH, RUSSIAN
+}
---
+++ src/main/java/com/nhsbt/bookstore/util/NumberGenerator.java
@@ -0,0 +1,15 @@
+package com.nhsbt.bookstore.util;
+
+/**
+ * @author Antonio Goncalves
+ *         http://www.antoniogoncalves.org
+ *         --
+ */
+public interface NumberGenerator {
+
+    // ======================================
+    // =          Business methods          =
+    // ======================================
+
+    String generateNumber();
+}
